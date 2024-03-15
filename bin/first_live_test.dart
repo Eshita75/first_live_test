825ee38773bd967017c6ff1605389d00becf8b76
Eshita75 <163004513+Eshita75@users.noreply.github.com>
2024-03-15 23:55:39 +0600

    updated

--- bin/first_live_test.dart
+++ bin/first_live_test.dart
@@ -1,18 +1,18 @@
 main(){
-  List<Fruit> fruits = [ //List with 3 fruits
+  List<Fruit> fruits = [ // Fruit List with 3 fruits
     Fruit('Apple', 'Red', 2.5),
     Fruit('Banana', 'Yellow', 1.0),
     Fruit('Grapes', 'Purple', 3.0),
   ];
 
   print('Original Fruit Details before Discount: ');
-  displayFruitDetails(fruits);
+  displayFruitDetails(fruits); // call the function for showing Original Fruit Details
 
   double discountPercentage = 10; // 10% discount
   applyPriceDiscount(fruits, discountPercentage);
 
   print('\nFruit Details After Applying ${discountPercentage.toInt()}% Discount:');
-  displayFruitDetails(fruits);
+  displayFruitDetails(fruits); // call the function for showing Fruit details after 10% discount;
 
 }
 
@@ -21,7 +21,7 @@ class Fruit{
   String color;
   double price;
 
-  Fruit(this.name, this.color, this.price);// Constructor of fruit class
+  Fruit(this.name, this.color, this.price);// Constructor of Fruit class
 }
 
 displayFruitDetails(List<Fruit> fruits) {
@@ -32,7 +32,7 @@ displayFruitDetails(List<Fruit> fruits) {
 
 applyPriceDiscount(List<Fruit> fruits, double discountPercentage) {
   for (var fruit in fruits) {
-    double discountedPrice = fruit.price - (fruit.price * (discountPercentage / 100));
+    double discountedPrice = fruit.price - (fruit.price * (discountPercentage / 100));// Calculate the price of fruit after 10% discount
     fruit.price = discountedPrice;
   }
 }
